29dee68731d98263f232fb881bcbf014748c4d31
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-03-13 14:30:58 +0000

    Display images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Post from './Post'
 
 const BASE_URL = 'http://localhost:8000/'
 
@@ -29,7 +30,15 @@ function App() {
   }, [])
 
   return (
-    'Hello World!'
+    <div className='app_posts'>
+      {
+        posts.map(post => (
+          <Post
+            post = {post}
+          />
+        ))
+      }
+    </div>
   );
 }
 
