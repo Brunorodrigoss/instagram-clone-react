00b1ce605d2d878cfe6c504f807cd8b695a594aa
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-04-01 16:08:25 +0100

    Upload image

--- src/App.js
+++ src/App.js
@@ -283,6 +283,8 @@ function App() {
       {
         authToken ? (
           <ImageUpload
+            authToken={authToken}
+            authTokenType={authTokenType}
           />
         ) : (
           <h3>You need to login to upload</h3>
